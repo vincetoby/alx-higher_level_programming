c51d15d8bb2a20e6d760beb949ee753e2755020a
vincetoby <vincetobechukwu@gmail.com>
2023-10-31 12:58:46 -0700

    update 0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for indx in range(97, 123):
+    print("{}".format(chr(indx)), end="")
+#97 - 122 is ASCII code for small alphabets
+#end="" makes it print without a newline character
+#chr function converts ASCII code to alphabet
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for indx in range(97, 123):
+    if indx != 101 and indx != 113:
+        print("{}".format(chr(indx)), end="")
