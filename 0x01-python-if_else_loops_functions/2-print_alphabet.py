9e42773f672bf17d79a4f9965dfa6d7ece210497
vincetoby <vincetobechukwu@gmail.com>
2023-10-31 13:01:38 -0700

    update 0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for indx in range(97, 123):
     print("{}".format(chr(indx)), end="")
-#97 - 122 is ASCII code for small alphabets
-#end="" makes it print without a newline character
-#chr function converts ASCII code to alphabet
