87e66d8f5b00291152820b9d77eae77d870242ca
vincetoby <vincetobechukwu@gmail.com>
2023-11-02 16:02:57 -0700

    update 0x02-python-import_modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+
+def calc(argv):
+    length = len(argv) - 1
+    if length != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    sym = argv[2]
+    b = int(argv[3])
+    if sym == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif sym == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif sym == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif sym == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+
+if __name__ == "__main__":
+    import sys
+    calc(sys.argv)
+
+
