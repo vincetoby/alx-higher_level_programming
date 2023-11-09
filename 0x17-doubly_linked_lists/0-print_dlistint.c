91b9c4c3c09a6700331c943b1cbc1fcb9081843b
vincetoby <vincetobechukwu@gmail.com>
2023-11-09 00:36:18 -0800

    update 0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,21 +10,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t len = 0;
 
-	if (h == NULL)
-		return (0);
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		len++;
-	}
-	if (len == 1)
-	{
-		printf("-> %lu element\n", len);
-	}
-	else
-	{
-		printf("-> %lu elements\n", len);
 	}
 	return (len);
 }
