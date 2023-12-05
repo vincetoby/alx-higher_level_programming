629fdff7adae234bc066c58cd52731169e305571
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-12-05 13:16:49 -0800

    update 0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-
+"""method module
+"""
 
 def read_file(filename=""):
+    """reads file"""
     with open(filename, "r", encoding="UTF-8") as opened_file:
         print(opened_file.read(), end="")
