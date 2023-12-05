ca550a90d3164a508fe894fc19012bd7e2f5af3a
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-12-04 17:54:58 -0800

    update 0x0A-python-inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,8 +6,8 @@ Method module
 
 def inherits_from(obj, a_class):
     """returns True if the object is an instance of a
-        class that inherited (directly or indirectly) 
-        from the specified class 
+        class that inherited (directly or indirectly)
+        from the specified class
     args:
         obj: object to access
         a_class: class type
