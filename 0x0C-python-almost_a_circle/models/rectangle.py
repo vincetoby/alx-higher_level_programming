929daf9bbbaa13471eccc687f66f8e92193f998a
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-12-11 04:31:47 -0800

    update 0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,3 +80,23 @@ class Rectangle(Base):
         """recturns info of rect"""
         return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__, self.id,\
                 self.x, self.y, self.width, self.height)
+
+     def __ups(self, id=None, width=None, height=None, x=None, y=None):
+        '''Internal method that updates instance attr via * or**args'''
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Updates instance attributes via no-keyword & keyword args"""
+        if args:
+            self.__ups(*args)
+        elif kwargs:
+            self.__ups(**kwargs
