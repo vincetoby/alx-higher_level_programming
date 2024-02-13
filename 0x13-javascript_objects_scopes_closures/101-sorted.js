d5c061e634b5ef4e5dc37d9f27f917032dd51647
vincetoby <vincetobechukwu@gmail.com>
2024-02-13 13:58:41 -0800

    update repo

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+let new_dict = {};
+for (let key in dict) {
+  if (new_dict[dict[key]] === undefined) {
+    new_dict[dict[key]] = [key];
+  } else {
+    new_dict[dict[key]].push(key);
+  }
+}
+console.log(new_dict);
