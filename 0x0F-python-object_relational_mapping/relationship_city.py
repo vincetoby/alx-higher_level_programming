e758d812b99da5e9aa0fdbf2c6243f1f0c121b62
vincetoby <vincetobechukwu@gmail.com>
2024-02-18 15:05:50 -0800

    update files

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-# this defines a class, City.
-# that Inherits from SQLAlchemy Base and links to the MySQL table cities.
-
+"""this defines a class, City.
+    that Inherits from SQLAlchemy Base and links to the MySQL table cities.
+"""
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
 
+Base = declarative_base()
 
 class City(Base):
     """class representing a city for a MySQL database.
