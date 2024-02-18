70a4db28ebd66deee69389776022a69bc44aad44
vincetoby <vincetobechukwu@gmail.com>
2024-02-18 14:45:28 -0800

    correct file errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-# Lists all states with a name starting with N from database hbtn_i0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""Lists all states with a name starting with N from database hbtn_i0e_0_usa.
+"""
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
